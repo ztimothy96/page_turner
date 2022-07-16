6e8aab0c7564339f053e0b30d06e9941268667a2
Timothy Zhou <z.timothy96@gmail.com>
2022-07-16 15:23:29 -0700

    Update stave_finder.py

--- stave_finder.py
+++ stave_finder.py
@@ -5,6 +5,14 @@ import os
 
 from PIL import Image
 
+def plot_vlines(im, lines, color='green'):
+    H = im.shape[0]
+    for i in range(len(lines)):
+        x = [lines[i], lines[i]]
+        y = [0, H-1]
+        plt.plot(x, y, color=color, linewidth=0.5)
+    plt.imshow(im)
+    plt.show()
 
 def plot_hlines(im, lines, color='blue'):
     W = im.shape[1]
@@ -15,20 +23,26 @@ def plot_hlines(im, lines, color='blue'):
     plt.imshow(im)
     plt.show()
 
-def find_staff_lines(im_bin, window = 3, thresh=0.2):
+def find_staff_lines(im_bin, window = 50, thresh=0.2):
     H = im_bin.shape[0]
     staff_lines = (np.mean(im_bin, axis=1) < thresh).nonzero()[0]
     whiteness = np.mean(im_bin, axis=0)
-    start_x = np.argmin(whiteness)
+    # get the x coordinate at the start of the staves
+    # this can actually be the end of the stave if the first stave is offset...
+    start_x = np.argmin(whiteness) 
+    plot_vlines(im_bin, [start_x])
+
+    # get the y coordinates at the top and bottom of each system 
     dif = im_bin[1:H, start_x] - im_bin[:H-1, start_x]
-    dif_i = (dif != 0).nonzero()[0]
+    dif_lines = (dif != 0).nonzero()[0] 
+    plot_hlines(im_bin, dif_lines)
 
     border_lines = []
-    for i in range(len(dif_i)):
-        closest = np.argmin((staff_lines - dif_i[i])**2)
-        if (staff_lines[closest] - dif_i[i])**2 < window**2:
+    for dif_line in dif_lines:
+        closest = np.argmin((staff_lines - dif_line)**2)
+        if (staff_lines[closest] - dif_line)**2 < window**2:
             border_lines.append(staff_lines[closest])
-    # plot_hlines(im_bin, border_lines, color='red')
+    plot_hlines(im_bin, border_lines, color='red')
     return border_lines
 
 def find_seps(im_bin, border_lines):
@@ -49,9 +63,9 @@ def sep_im(file_path, count=1):
     im_bin = cv2.threshold(im_bin*1.0/255, thresh, 1.0, cv2.THRESH_BINARY)[1]
 
     border_lines = find_staff_lines(im_bin)
-    # plot_hlines(im_bin, border_lines)
+    plot_hlines(im_bin, border_lines)
     seps = find_seps(im_bin, border_lines)
-    # plot_hlines(im_bin, seps)
+    plot_hlines(im_bin, seps)
 
     staves = np.split(im, seps, axis=0)
     for i in range(len(staves)):       
@@ -69,11 +83,8 @@ def sep_folder(root, base_name):
     return True
 
 
-base_name = 'violin_sonata'
+base_name = 'rigaudon'
 root = './'
 
 sep_folder(root, base_name)
-
-
-
 
